c5ef04228df99eef5e168007a2cbd973c35932e3
Esteban Manchado Velázquez <emanchado@demiurgo.org>
2014-04-12 23:55:10 +0200

    First RoboHydra tests refactoring step

---
+++ test/robohydra/plugins/pim-dav/scenarios/redirect.js
@@ -0,0 +1,16 @@
+var RoboHydraHead = require("robohydra").heads.RoboHydraHead;
+
+exports.getBodyParts = function(/*conf*/) {
+    return {
+        heads: [
+            new RoboHydraHead({
+                path: "/redirect",
+                handler: function(req,res) {
+                    res.statusCode = 302;
+                    res.headers.location = 'http://www.example.com';
+                    res.end();
+                }
+            })
+        ]
+    };
+};
